51e953b128fd0b8b58d7a16f6ef79f5c79fdeb79
greatwaves11 <1125059790@qq.com>
2023-05-03 21:39:14 +0800

    a new file

---
+++ WordCount.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	FILE *fp;
+	int num = 0;
+	char c;
+	int start = 0;
+	int end = 0;
+
+	fp = fopen(argv[2], "r");
+
+	if (fp == NULL) {
+		printf("无法打开%s\n", argv[2]);
+		exit(1);
+	} else {
+		if (argv[1][1] == 'c') {
+			while (c != EOF) {
+				c = fgetc(fp);
+				num++;
+			}
+			printf("字符数为：%d\n", num);
+		} else if (argv[1][1] == 'w') {
+			while (c != EOF) {
+				c = fgetc(fp);
+				if (c != ' ' && c != ',' && c != '\n') {
+					start = 1;
+//					printf("start=%d", start);
+				} else if (c == ' ' || c == ',' || c == '\n') {
+					if (start == 1) {
+						end = 1;
+//						printf("end=%d", end);
+					}
+				}
+				if (start == 1 && end == 1) {
+					num++;
+					start = 0;
+					end = 0;
+				}
+			}
+			end = 1;
+			if (start == 1 && end == 1) {
+				num++;
+			}
+			printf("单词数为：%d\n", num);
+		} else {
+			printf("输入参数错误\n");
+		}
+	}
+}
